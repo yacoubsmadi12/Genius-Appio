9a249bc34aa3a809e1a6ebda514c69cbab84f58a
yakup al-smadi <yakupsmadi@gmail.com>
2025-09-16 18:30:31 +0000

    خلص الغي الصور يلي رفعتهم انا

--- next.config.ts
+++ next.config.ts
@@ -28,12 +28,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'i.postimg.cc',
-        port: '',
-        pathname: '/**',
-      },
     ],
   },
 };
