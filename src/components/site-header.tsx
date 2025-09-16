f77b5ea45d781d8efd519c08e58254e03bc29c6d
yakup al-smadi <yakupsmadi@gmail.com>
2025-09-16 16:47:34 +0000

    in center i need not on right

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -38,10 +38,12 @@ export function SiteHeader() {
 
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center justify-between">
-        <div className="flex items-center gap-6">
+      <div className="container flex h-16 items-center">
+        <div className="flex items-center">
           <Logo />
-          <MainNav />
+        </div>
+        <div className="flex-1 flex justify-center">
+            <MainNav />
         </div>
         <div className="flex items-center justify-end space-x-2">
           <DropdownMenu>
